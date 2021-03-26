470ab738eee4a9ea40bb33f9d7b2c26ae131262a
Cemkocabas <cem.kocabas@megacode.com.tr>
2021-03-26 15:24:38 +0300

    update
    update

---
+++ Services/UserServices.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Services
+{
+
+    public  interface IUserServices
+    {
+        string GetUser(string username);
+    }
+    public class UserServices : IUserServices
+    {
+        public string GetUser(string username)
+        {
+            return username;
+        }
+    }
+}
